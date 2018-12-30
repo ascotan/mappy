e49a91aefa38bce2d9d36c8f54e536d2b902b054
Joseph Gilbert <joseph.gilbert@o3bnetworks.com>
2018-12-30 10:36:04 -0500

    updates

--- src/map.js
+++ src/map.js
@@ -6,30 +6,31 @@ import {Delaunay} from "d3-delaunay";
 var MapGenerator = (function() {
     var relaxIterations = 2;
 
-    // create a set of points for the voroni diagram
-    function generatePoints(n, extent) {
+    function createRandomPoints(n, extent) {
+        // create a random set of points
         var points = [];
         for (var i = 0; i < n; i++) {
             points.push([Math.random() * extent.width, Math.random() * extent.height]);
         }
-
-        // lloyd's relaxation to improve the voroni diagram
-        // do 2 iterations
-        for (let x=0; x < relaxIterations; x++) {
-            var generator = voroni(points, extent).cellPolygons();
-            points = [];
-            var poly = generator.next()
-            while (poly.done != true) {
-              points.push(centroid(poly.value));
-              poly = generator.next();
-            }
-        }
-
         return points
     }
 
-    // calculate the centroid of a polygon
-    function centroid(polygon) {
+    // do a few iterations of lloyd's relaxation to imporove the centroids
+    function relaxPoints(points, extent) {
+        for (let x=0; x < relaxIterations; x++) {
+            var generator = generateVoroniDiagram(points, extent).cellPolygons();
+            points = [];
+            var poly = generator.next()
+            while (poly.done != true) {
+              points.push(calculateCentroid(poly.value));
+              poly = generator.next();
+            }
+        }
+        return points
+    }
+
+    //calculate the centroid of a polygon
+    function calculateCentroid(polygon) {
       var i = -1, n = polygon.length, x = 0, y = 0, a, b = polygon[n - 1], c, k = 0;
       while (++i < n) {
         a = b;
@@ -42,59 +43,47 @@ var MapGenerator = (function() {
       return k *= 3, [x / k, y / k];
     }
 
-    // generate a voroni diagram
-    function voroni(points, extent) {
+    // generate a voroni diagram from a set of points
+    function generateVoroniDiagram(points, extent) {
         const d = Delaunay.from(points);
         return d.voronoi([0, 0, extent.width, extent.height]);
     }
 
-    function heightToColor(height) {
-        // max height = 1000, min height = -1000
-        var r = 125 + (125 * (height * .001));
-        var g = 125 + (125 * (height * .001));
-        var b = 125 + (125 * (height * .001));
-        return "#" + (r).toString(16) + (g).toString(16) + (b).toString(16);
-    }
+    // build a datastructure to represent the voroni mesh
+    function buildBaseMesh(points, extent) {
+        var mesh = {
+            voroni: null,
+            polys: [],
+            dxhash: {},
+            dyhash: {}
+        };
 
-    function randomPolyIndex(mesh) {
-        return Math.round(Math.random() * mesh.polys.length);
-    }
+        // add all the polygons
+        var polys = [];
+        mesh.voroni = generateVoroniDiagram(points, extent);
+        var generator = mesh.voroni.cellPolygons();
+        var poly = generator.next()
+        while (poly.done != true) {
+          polys.push(poly.value);
+          poly = generator.next();
+        }
 
-    function neighbors(mesh, index) {
-        var poly = mesh.polys[index];
-        var dIndex = mesh.voroni.delaunay.find(poly.centroid[0], poly.centroid[1]);
-        var generator =  mesh.voroni.delaunay.neighbors(dIndex);
-
-        var centroids = [];
-        var n = generator.next();
-        while (n.done != true) {
-            mesh.voroni.delaunay.trianglePolygon(n.value).forEach((centroid) => {
-                centroids.push(centroid);
+        // each polygon has a centroid, a set of points to define
+        // it's hull and a height (for a heightmap)
+        polys.forEach((pnts, index) => {
+            mesh.polys.push({
+                centroid: points[index],
+                points: pnts,
+                height: 0
             });
-            n = generator.next();
-        }
-        centroids = _.uniqWith(centroids, _.isEqual);
+        })
 
-
-
-        var node = mesh.voroni.delaunay.hull;
-        var sIndex = node.i;
-        console.log(poly.centroid);
-        console.log(node);
-        while (node.i != sIndex) {
-            console.log("HERE");
-            if (node.x == poly.centroid[0] && node.y == poly.centroid[1]) {
-                console.log("MATCH");
-                console.log(node);
-                break;
-            }
-            node = node.next;
-        }
-
-
-        return centroids;
-
-
+        // create a hashtable for centroids based on delta x and y
+        mesh.polys.forEach((poly, index) => {
+            mesh.dxhash[poly.centroid[0]] = index;
+            mesh.dyhash[poly.centroid[1]] = index;
+        });
+        return mesh;
     }
 
     function addBump(mesh) {
@@ -104,31 +93,38 @@ var MapGenerator = (function() {
         return mesh, n
     }
 
+    function randomPolyIndex(mesh) {
+        return Math.round(Math.random() * mesh.polys.length);
+    }
+
+    function neighbors(mesh, index) {
+        var poly = mesh.polys[index];
+        var neighbors = [];
+        mesh.polys.forEach((other, i) => {
+            if (_.intersection(other.points, poly.points).length > 0) {
+                console.log(other);
+                neighbors.push(i);
+            }
+        })
+        return neighbors;
+    }
+
+    // get a hex color from a height
+    // assumes that heights are clamped to -1000 to 1000
+    function heightToColor(height) {
+        var r = 125 + (125 * (height * .001));
+        var g = 125 + (125 * (height * .001));
+        var b = 125 + (125 * (height * .001));
+        return "#" + (r).toString(16) + (g).toString(16) + (b).toString(16);
+    }
+
     return {
         GeneratePoints: (number, extent) => {
-            return generatePoints(number, extent);
+            return relaxPoints(
+                createRandomPoints(number, extent), extent);
         },
         GenerateMesh: (points, extent) => {
-            var mesh = {
-                voroni: null,
-                polys: []
-            };
-            var polys = [];
-            mesh.voroni = voroni(points, extent);
-            var generator = mesh.voroni.cellPolygons();
-            var poly = generator.next()
-            while (poly.done != true) {
-              polys.push(poly.value);
-              poly = generator.next();
-            }
-            polys.forEach((pnts, index) => {
-                mesh.polys.push({
-                    centroid: points[index],
-                    points: pnts,
-                    height: 0
-                });
-            })
-            return mesh;
+            return buildBaseMesh(points, extent);
         },
         GenerateHeightMap: (mesh) => {
             mesh = addBump(mesh);
